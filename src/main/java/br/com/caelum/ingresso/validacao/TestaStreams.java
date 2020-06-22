18a2e0357116e43065002d07d6ba808d5142e065
Luciano Mendes Almeida <lucalm@gmail.com>
2020-06-22 19:07:17 -0300

    Garantindo que a validacao de horarios para cadastrar uma sessao esta correta

---
+++ src/main/java/br/com/caelum/ingresso/validacao/TestaStreams.java
@@ -0,0 +1,49 @@
+package br.com.caelum.ingresso.validacao;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.caelum.ingresso.model.Filme;
+
+public class TestaStreams {
+
+	public static void main(String[] args) {
+		
+		Filme f1 = new Filme("Terminator", Duration.ofHours(1), "Ação");
+		Filme f2 = new Filme("Predador", Duration.ofHours(1), "Drama");
+		Filme f3 = new Filme("Duro de Matar", Duration.ofHours(1), "Drama");
+
+		List<Filme> filmes = Arrays.asList(f1, f2, f3);
+		
+		System.out.println("Filmes:");
+		filmes.stream().map(f -> f.getNome()).forEach(System.out::println);
+//		filmes.stream().forEach(f -> System.out.println(f.getNome()));
+//		for (Filme f : filmes) {
+//			System.out.println(f.getNome());
+//		}
+		
+//		List<Filme> dramas = new ArrayList<>();
+//		for (Filme f : filmes) {
+//			if("Drama".equals(f.getGenero())) {
+//				dramas.add(f);
+//			}
+//		}
+		
+		List<Filme> dramas = filmes.stream().filter(f -> "Drama".equals(f.getGenero()))
+				.collect(Collectors.toList());
+		
+		System.out.println("Dramas:");
+		filmes.stream()
+		.filter(f -> "Drama".equals(f.getGenero()))
+		.map(f -> f.getNome())
+		.sorted()
+		.forEach(System.out::println);
+		
+//		for (Filme f : dramas) {
+//			System.out.println(f.getNome());
+//		}
+	}
+
+}
---
+++ src/main/java/br/com/caelum/ingresso/validacao/TestadorDeGerenciadorDeSessao.java
@@ -0,0 +1,53 @@
+package br.com.caelum.ingresso.validacao;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+
+public class TestadorDeGerenciadorDeSessao {
+
+	public static void main(String[] args) {
+
+		// Teste 1
+		Filme f = new Filme("Terminator", Duration.ofHours(1), "Ação");
+		Sala s = new Sala("Sala VIP");
+		Sessao sessaoNova = new Sessao(LocalTime.of(22, 0), f, s);
+		List<Sessao> sessoes = new ArrayList<>();
+		GerenciadorDeSessao ger = new GerenciadorDeSessao(sessoes);
+		System.out.println(ger.cabe(sessaoNova) == true ? "ok" : "falhou");
+
+		// Teste 2
+		f = new Filme("Terminator", Duration.ofHours(3), "Ação");
+		s = new Sala("Sala VIP");
+		sessaoNova = new Sessao(LocalTime.of(22, 0), f, s);
+		sessoes = new ArrayList<>();
+		ger = new GerenciadorDeSessao(sessoes);
+		System.out.println(ger.cabe(sessaoNova) == false ? "ok" : "falhou");
+
+		// Teste 3
+		f = new Filme("Terminator", Duration.ofHours(1), "Ação");
+		s = new Sala("Sala VIP");
+		sessaoNova = new Sessao(LocalTime.of(22, 0), f, s);
+		Sessao sessaoAtual = new Sessao(LocalTime.of(19, 0), f, s);
+		sessoes = Arrays.asList(sessaoAtual);
+		ger = new GerenciadorDeSessao(sessoes);
+		System.out.println(ger.cabe(sessaoNova) == true ? "ok" : "falhou");
+
+		// Teste 4
+		f = new Filme("Terminator", Duration.ofHours(1), "Ação");
+		s = new Sala("Sala VIP");
+		sessaoNova = new Sessao(LocalTime.of(22, 0), f, s);
+		sessaoAtual = new Sessao(LocalTime.of(21, 30), f, s);
+		sessoes = Arrays.asList(sessaoAtual);
+		ger = new GerenciadorDeSessao(sessoes);
+		System.out.println(ger.cabe(sessaoNova) == false ? "ok" : "falhou");
+
+	}
+
+}
