aaa23f56a690a26949ce2289631c7af93cd5e703
Luciano Mendes Almeida <lucalm@gmail.com>
2020-06-24 21:03:11 -0300

    Implementando a selecao de lugares, ingressos e tipos de ingressos

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,19 +3,25 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Sessao {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	private LocalTime horario;
 	@ManyToOne
 	private Sala sala;
@@ -23,15 +29,27 @@ public class Sessao {
 	private Filme filme;
 	private BigDecimal preco;
 
+	@OneToMany(mappedBy = "sessao", fetch = FetchType.EAGER)
+	private Set<Ingresso> ingressos = new HashSet<>();
+
 	public Sessao() {
-		
+
 	}
-	
+
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
 		this.preco = sala.getPreco().add(filme.getPreco());
+	}
+
+	public boolean isDisponivel(Lugar lugarSelecionado) {
+		return ingressos.stream().map(Ingresso::getLugar).
+				noneMatch(lugar -> lugar.equals(lugarSelecionado));
+	}
+
+	public Map<String, List<Lugar>> getMapaDeLugares() {
+		return sala.getMapaDeLugares();
 	}
 
 	public Integer getId() {
@@ -65,7 +83,7 @@ public class Sessao {
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
-	
+
 	public BigDecimal getPreco() {
 		return preco.setScale(2, RoundingMode.HALF_UP);
 	}
