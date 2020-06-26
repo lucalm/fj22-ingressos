c974237171b86af6d1025959a6edd910672c0c99
Luciano Mendes Almeida <lucalm@gmail.com>
2020-06-25 22:46:12 -0300

    Implementacao de UserDetails, UserDetailsservice e GrantedAuthority (rev01)

--- src/main/java/br/com/caelum/ingresso/configuracao/SecurityConfiguration.java
+++ src/main/java/br/com/caelum/ingresso/configuracao/SecurityConfiguration.java
@@ -25,11 +25,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN").antMatchers("/compra/**")
-				.hasRole("COMPRADOR").antMatchers("/filme/**").permitAll().antMatchers("/sessao/**/lugares").permitAll()
-				.antMatchers("/magic/**").permitAll().antMatchers("/").permitAll().anyRequest().authenticated().and()
-				.formLogin().usernameParameter("email").loginPage("/login").permitAll().and().logout()
-				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll();
+		http
+		.csrf().disable().authorizeRequests()
+		.antMatchers("/admin/**").hasRole("ADMIN")
+		.antMatchers("/compra/**").hasRole("COMPRADOR")
+		.antMatchers("/filme/**").permitAll()
+		.antMatchers("/sessao/**/lugares").permitAll()
+		.antMatchers("/magic/**").permitAll()
+		.antMatchers("/").permitAll()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.formLogin()
+		.usernameParameter("email")
+		.loginPage("/login")
+		.permitAll()
+		.and()
+		.logout()
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		.permitAll();
 	}
 
 	@Override
